c0e3d50278bc5931b86cd8ad2375418dcc7886e0
Rami <rataxes.c@gmail.com>
2024-07-27 14:13:47 +0200

    fix caskadia font path.

--- install/desktop/app-alactritty.sh
+++ install/desktop/app-alactritty.sh
@@ -2,5 +2,5 @@ sudo apt install -y alacritty
 mkdir -p ~/.config/alacritty
 cp ~/.local/share/rkc-pc/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
 cp ~/.local/share/rkc-pc/themes/tokyo-night/alacritty.toml ~/.config/alacritty/theme.toml
-cp ~/.local/share/rkc-pc/configs/alacritty/fonts/CaskaydiaMono.toml ~/.config/alacritty/font.toml
+cp ~/.local/share/rkc-pc/configs/alacritty/fonts/CaskadiaMono.toml ~/.config/alacritty/font.toml
 cp ~/.local/share/rkc-pc/configs/alacritty/font-size.toml ~/.config/alacritty/font-size.toml
