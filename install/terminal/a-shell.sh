192a564532a3998a7969ea8593d92f98362df006
Rami <rataxes.c@gmail.com>
2024-07-30 14:43:33 +0200

    update PATH for installers.

--- install/terminal/a-shell.sh
+++ install/terminal/a-shell.sh
@@ -2,6 +2,9 @@
 [ -f "~/.bashrc" ] && mv ~/.bashrc ~/.bashrc.bak
 cp ~/.local/share/rkc-pc/configs/bashrc ~/.bashrc
 
+# Load the PATH for use later in the installers
+source ~/.local/share/rkc-pc/defaults/bash/shell
+
 [ -f "~/.inputrc" ] && mv ~/.inputrc ~/.inputrc.bak
 # Configure the inputrc using Omakub defaults
 cp ~/.local/share/rkc-pc/configs/inputrc ~/.inputrc
